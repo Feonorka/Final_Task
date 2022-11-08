19f5167ba45f08b34cfbb9ef14e7d32f2cebd8c7
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-11-08 23:46:33 +0300

    Добавлены методы для генерации

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[num];
+
+
+
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray(array);
+array = CreateArrayStr(num, RandomNum(0, 5), RandomNum(0,5));
+PrintArray(array);
+
+
+// Генерация случайной строки
+string RandomString(int length)
+        {
+            var result = new char[length];
+            var r = new Random();
+            for (int i = 0; i < result.Length; i++)
+            {
+                do
+                    result[i] = (char) r.Next(100); 
+                while (result[i] < '!');
+            }
+            return new string(result);
+        }
+// Генерация случайного числа для задания длины случайной строки
+int RandomNum(int min, int max)
+{
+    int num = default;
+    num = new Random().Next(min, max);
+    return num;
+}
+
+
+string[] CreateArrayStr(int size, int min, int max)
+{
+    string[] array = new string[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = RandomString(size);
+    }
+    return array;
+}
